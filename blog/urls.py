cf3e4fd21455d8f6880ade3e3fe9f3b277acdb83
muhamadzade <muhamadzade.gmail.com>
2022-07-11 19:18:06 +0430

    phase9

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('create/',views.create_post,name='create_post'),
+    path('create/', views.create_post, name='create_post'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
