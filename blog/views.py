683ebb58d57db11e0e017a86963d873073e2ee6d
muhamadzade <muhamadzade.gmail.com>
2022-07-11 16:34:28 +0430

    phase3

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 
-def printH():
-    pass
+def post_list(requests):
+    return HttpResponse("post_list")
