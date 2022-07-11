7029f1d663afc18aad0af39b9b84d5f2d8758375
muhamadzade <muhamadzade.gmail.com>
2022-07-11 16:54:21 +0430

    phase4

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def post_list(requests):
-    return HttpResponse("post_list")
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
